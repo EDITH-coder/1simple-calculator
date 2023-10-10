89676d89a011f54b86fc2d70122f7f65b5d010b5
EDITH_Coder <edithmashapo@gmail.com>
2023-10-10 08:59:09 +0200

    Add project files.

---
+++ 1simple calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1simple_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            double num1= Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter an operator");
+            String op = Console.ReadLine();
+
+            Console.WriteLine("Enter second number");
+            double num2= Convert.ToDouble(Console.ReadLine());
+
+            if(op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }else if(op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }else if(op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else {
+                Console.WriteLine("Not an operator");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
